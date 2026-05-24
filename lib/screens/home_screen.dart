e65194eae6dd885b7912d6dea7ae93de1fe5a52c
ShAhD AhMeD <shahd55ahmed2005@gmail.com>
2026-05-24 23:54:16 +0300

    database

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,7 +6,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: AppBar(
-          title: Text("hi in sekka app your friend in your way"),
+          title: Text("hi!!! in sekka app your friend in your way"),
         ),
     );
   }
